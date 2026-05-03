995ffefe26744fa0282d5526de9572236a22fbe4
AtikHasanSarker <atiksagor24@gmail.com>
2026-05-03 13:33:58 +0600

    Added Authenticatio

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -9,6 +9,7 @@ const CoursesPage = async () => {
     return (
       <div className='max-w-7xl mx-auto px-6'>
         <h2 className='text-center text-4xl font-bold my-10'>All Courses</h2>
+        
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           {courses.map((course) => (
             <CourseCard key={course.id} course={course} />
